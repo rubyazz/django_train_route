01234b70d5d7bef701b2f9ece5396d1a732bec65
rubyazz <ersultan.abduvalov@gmail.com>
2022-05-17 10:35:35 +0600

    templating and creating forms

--- cities/views.py
+++ cities/views.py
@@ -1,8 +1,32 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+
+from cities.forms import HtmlForm, CityForm
+from cities.models import City
 
 
+__all__ = (
+    'home', 'CityDetailView'
+)
 
 
-def index(request):
-    return render(request, "base.html")
+def home(request, pk=None):
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    # if pk:
+        # city = City.objects.filter(id=pk).first()
+        # city = get_object_or_404(City, id=pk)
+        # context = {'object': city}
+        # return render(request, 'cities/detail.html', context)
+    form = CityForm()
+    qs = City.objects.all()
+    context = {'objects_list': qs, 'form': form}
+    return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
