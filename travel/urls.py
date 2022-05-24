f0dd536071af103b81bb38d45e993129e66eea7d
rubyazz <ersultan.abduvalov@gmail.com>
2022-05-24 12:14:35 +0600

    finished project V2

--- travel/urls.py
+++ travel/urls.py
@@ -15,12 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from routes.views import home, find_routes
+
+from routes import views
+from routes.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
+    path('accounts/', include(('accounts.urls', 'accounts'))),
     path('', home, name='home'),
-    path('find_routes/', find_routes, name='find_routes')
+    path('find_routes/', find_routes, name='find_routes'),
+    path('add_route/', add_route, name='add_route'),
+    path('save_route/', save_route, name='save_route'),
+    path('list/', RouteListView.as_view(), name='list'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', RouteDeleteView.as_view(), name='delete'),
 ]
